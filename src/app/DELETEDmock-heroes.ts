19f44550a99cd13a9e4d7e80bf20876cf34f8a35
Alexander Studer <alex@alexstuder.ch>
2017-12-05 23:30:32 +0100

    Erste Version

---
+++ src/app/DELETEDmock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    {id:11, name:'Jimi Hendrix'}
+    ,{id:12, name:'Robert Johnson'}
+    ,{id:13, name:'B.B.King'}
+    ,{id:13, name:'Steve Winwood'}
+    ,{id:14, name:'David Gilmore'}
+    ,{id:15, name:'Joe Bonamassa'}
+    ,{id:16, name:'Eric Clapton'}
+    ,{id:17, name:'Steve Ray Vaughan'}
+    ,{id:18, name:'Joe Satriani'}
+    ,{id:19, name:'Walter Trout'}
+    ,{id:20, name:'CarlosSantana'}
+];
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  // styleUrls: ['./styles.css']
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
